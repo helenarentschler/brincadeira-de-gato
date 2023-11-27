afbd400962bfe60830cd0d0a8ca3712bf5b8bace
Helena Rentschler <helrentschler@gmail.com>
2023-11-26 21:48:02 -0300

    final

--- scripts/mudarObstaculo.js
+++ scripts/mudarObstaculo.js
@@ -3,7 +3,7 @@ export default function mudarObstaculo(obstaculo) {
     //o obstaculo esta completamnete (toda sua largura) fora da tela a esquerda
     if(obstaculo.getBoundingClientRect().left < -190) {
         
-        const i = Math.floor(Math.random() * 3) + 1;
+        const i = Math.floor(Math.random() * 6) + 1;
         obstaculo.setAttribute("src", `imagens/obstaculos/${i}.png`);
     }
 }
